9c852b4d35329b37a098167cca8cdc86f29a6841
TigiGln <thierry.galliano@etu.univ-amu.fr>
2021-11-24 20:20:15 +0100

    deploy

--- quizz_biologic/settings.py
+++ quizz_biologic/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'quizz_biologic.urls'
