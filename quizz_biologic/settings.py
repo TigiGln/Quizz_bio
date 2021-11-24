ccbf1d5b63f88e3f719e9f0ad466fa66cd3d7b7e
TigiGln <thierry.galliano@etu.univ-amu.fr>
2021-11-24 21:03:13 +0100

    Revert "deploy"
    This reverts commit 83380907e5ea23893db32853b5219f1d4682e99d.

--- quizz_biologic/settings.py
+++ quizz_biologic/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,12 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g#(*=$2ka^z%c#hdb8ch=up-&fs)*i($6l@n&dzc8yyyc%3c&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://quizbiologic.herokuapp.com/", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -132,7 +128,3 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_REDIRECT_URL = '/'
-
-if os.environ.get('ENV') == 'PRODUCTION':
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    django_heroku.settings(locals())
